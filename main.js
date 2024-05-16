5f9117880de3102f3857b2ce4404072a63da7482
Luana Vitória Nerilo Bilha De Souza <luana.bilha.souza@escola.pr.gov.br>
2024-05-16 08:08:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+console.log(botoes)
+for (let i = 0; i < botoes.length ; i++){
+   botoes[i].onclick = fuction() { 
+ for (j=0;j<botoes.length;j++){
+    botoes[j].classList.remove("ativo");
+ }
+ botoes[i].classList.add("ativo")
+}
+   
+}
