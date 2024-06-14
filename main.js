0492073f6fdb083b3522f2f04ee3dd1c3193836d
Luana Vitória Nerilo Bilha De Souza <luana.bilha.souza@escola.pr.gov.br>
2024-06-14 09:43:40 -0300

    14/06

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
+const conteudo =document.querySelectorAll(".aba-conteudo")
+const tempo=  document.querySelectorAll(".contador")
 console.log(botoes)
+tempo[O].textContent="Luana"
+const tempoObjetivo1=new date(12/08/2024,13,08);
+const tempoObjetivo2=new date(12/09/2024,20,10);
+const tempoObjetivo3=new date(02/10/2024,15,07);
+const tempoObjetivo4=new date(13/11/2024,08,19);
+const tempoObjetivo5=new date(01/12/2024,12,09);
+const agora=new date();
+tempo[1].textContent=agora
 for(let i=0; i <botoes.length; i++) {
  botoes[i].onclick = function(){
    for(j=0; j<botoes.length; j++){
@@ -9,4 +19,5 @@ for(let i=0; i <botoes.length; i++) {
 botoes[i].classList.add("ativo");
 conteudo[i].classList.add("ativo");
  }
+
 }
