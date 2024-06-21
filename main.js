ed6ad665e59d2c0eba451b1933d643a25433bd72
Luana Vitória Nerilo Bilha De Souza <luana.bilha.souza@escola.pr.gov.br>
2024-06-21 09:43:01 -0300

    21/06

--- main.js
+++ main.js
@@ -10,6 +10,24 @@ const tempoObjetivo4=new date(13/11/2024,08,19);
 const tempoObjetivo5=new date(01/12/2024,12,09);
 const agora=new date();
 tempo[1].textContent=agora
+let segundos;
+let minutos;
+let horas;
+let dias;
+Segundos = (tempoObjetivo1 - agora)/1000
+Minutos = segundos/60
+Horas = minutos/60
+Dias = horas/24
+segundos=math.floor(Segundos)
+minutos=math.floor(minutos)
+horas=math.floor(horas)
+dias=math.floor(dias)
+tempo[0].textContent=`faltam(dias)
+(segundos).segundos;
+(horas).horas;
+(minutos).minutos.
+tempo[0].textContent = tempoObjetivo1 - agora
+tempo[1].textContent = tempoObjetivo1
 for(let i=0; i <botoes.length; i++) {
  botoes[i].onclick = function(){
    for(j=0; j<botoes.length; j++){
